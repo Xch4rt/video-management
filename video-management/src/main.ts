5b8dac614d87c4621bf905db1b0dc8d7f7d83b38
Xch4rt <pab203.guti@gmail.com>
2024-02-28 08:22:09 -0600

    configurations and swagger doc

--- video-management/src/main.ts
+++ video-management/src/main.ts
@@ -1,8 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { useContainer } from 'class-validator';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter());
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }));
+
+  useContainer(app.select(AppModule), {
+    fallbackOnErrors: true
+  });
+
+  const logger = new Logger('Bootstrap');
+
+  const config = new DocumentBuilder()
+    .setTitle('Video Management API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || 3000;
+
+  app.enableCors({
+    origin: '*'
+  });
+
   await app.listen(3000);
+
+  logger.log(`API listening on port ${port}`);
 }
 bootstrap();
